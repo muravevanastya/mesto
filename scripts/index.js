b927946aab4220d77692b751880ec8335feaf9a9
muravevanastya <muravevanastya@gmail.com>
2022-11-04 11:32:05 +0200

    feat: реализовать удаление

--- scripts/index.js
+++ scripts/index.js
@@ -31,32 +31,33 @@ const formElement = document.querySelector('.popup__form');
 
 const elementsTemplate = document.querySelector('.elements-template').content;
 const elements = document.querySelector('.elements');
+// const element = elementsTemplate.querySelector('.element');
 
 
 const initialCards = [
   {
-    name: 'Мёртвое море',
-    link: 'https://avatars.mds.yandex.net/get-images-cbir/985591/3YG0Sw62UxB6SPjE6cI-yg6146/ocr'
-  },
-  {
-    name: 'Гранд-Каньон',
-    link: 'https://avatars.mds.yandex.net/get-images-cbir/2130213/-F3PYbpfZhrYZczaXTtrGA6065/ocr'
-  },
-  {
-    name: 'Утёсы Мохер',
-    link: 'https://pibig.info/uploads/posts/2022-03/thumbs/1646705299_17-pibig-info-p-otvesnaya-skala-priroda-krasivo-foto-26.jpg'
-  },
-  {
-    name: 'Вулкан Бромо',
-    link: 'https://sun9-83.userapi.com/impf/1q7uw-Az3foBorQqZgSYgFu_zcK5FhHs2M2rQg/zZvS4yIrvjw.jpg?size=1600x956&quality=96&sign=28753ee71b1a80032fa55287cb035520&c_uniq_tag=t9BW8yE4YoUAbYOor_Ad5bU0XZL9mYP-dW3tu7BsDV8&type=album'
+    name: 'Памуккале',
+    link: 'https://www.nationalturk.com/en/wp-content/uploads/2022/05/pamukkale-turkey.jpg'
   },
   {
     name: 'Озеро Спенсер',
     link: 'https://ifbest.org/uploads/images/2021/11/image_750x_619a50a9df9e8.jpg'
   },
   {
-    name: 'Памуккале',
-    link: 'https://www.nationalturk.com/en/wp-content/uploads/2022/05/pamukkale-turkey.jpg'
+    name: 'Вулкан Бромо',
+    link: 'https://sun9-83.userapi.com/impf/1q7uw-Az3foBorQqZgSYgFu_zcK5FhHs2M2rQg/zZvS4yIrvjw.jpg?size=1600x956&quality=96&sign=28753ee71b1a80032fa55287cb035520&c_uniq_tag=t9BW8yE4YoUAbYOor_Ad5bU0XZL9mYP-dW3tu7BsDV8&type=album'
+  },
+  {
+    name: 'Утёсы Мохер',
+    link: 'https://pibig.info/uploads/posts/2022-03/thumbs/1646705299_17-pibig-info-p-otvesnaya-skala-priroda-krasivo-foto-26.jpg'
+  },
+  {
+    name: 'Гранд-Каньон',
+    link: 'https://planettravel24.com/wp-content/uploads/2018/01/bolshoj-kanon-arizona.jpg'
+  },
+    {
+    name: 'Мёртвое море',
+    link: 'https://traveltimes.ru/wp-content/uploads/2021/07/1020x0_unbvkyltrihbtvfm_jpg_9a38.jpg'
   },
 ];
 
@@ -103,16 +104,21 @@ addCloseButton.addEventListener('click', () => {
 });
 
 
-// function setEventListeners(el) {
-//   const deleteButton = el.querySelector('.element__delete');
-//   deleteButton.addEventListener('click', deleteHandler);
 
-//   const likeButton = el.querySelector('.element__like');
-//   likeButton.addEventListener('click', likeHandler);
 
-//   const elementImage = el.querySelector('.element__image');
-//   elementImage.addEventListener('click', imageOpenHandler)
-// };
+
+
+
+function elementSubmitHandler(evt) {
+  evt.preventDefault();
+
+  const card = addElement(placeInput.value, imageInput.value);
+
+  elements.prepend(card);
+  // renderCard(placeInput.value, imageInput.value);
+  popupClose(popupAdd);
+};
+
 
 
 function addElement(name, link) {
@@ -121,60 +127,44 @@ function addElement(name, link) {
   const newElementTitle = newElement.querySelector('.element__title');
   const newElementImage = newElement.querySelector('.element__image');
 
+  const deleteButton = newElement.querySelector('.element__delete');
+  const likeButton = newElement.querySelector('.element__like');
+
+
   newElementTitle.textContent = name;
   newElementImage.alt = name;
   newElementImage.src = link;
 
-  // setEventListeners(newElement);
+  deleteButton.addEventListener('click', () => {
+    newElement.remove();
+  });
+
+  likeButton.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__like:active');
+  });
+
   return newElement;
 };
 
-function elementSubmitHandler(evt) {
-  evt.preventDefault();
+// function renderCard(name, link) {
+//   const card = addElement(name, link);
+//   elements.prepend(card);
+// };
 
-  const card = addElement(placeInput.value, imageInput.value);
+initialCards.forEach((item) => {
+  const card = addElement(item.name, item.link);
   elements.prepend(card);
-  popupClose(popupAdd);
-};
-
-initialCards.forEach(function(item) {
-  addElement(item.name, item.link);
 });
 
+
+
 createAddButton.addEventListener('click', elementSubmitHandler);
-addForm.addEventListener('submit', elementSubmitHandler);
-
-// addOpenButton.addEventListener('click', function () {
-//   const name = document.querySelector('.popup__input_type_name');
-//   const link = document.querySelector('.popup__input_type_job');
-
-//   addElement(name.value, link.value);
-
-//   name.value = '';
-//   link.value = '';
-// });
+popupAdd.addEventListener('submit', elementSubmitHandler);
 
 
-
-
-// function elementDeleteHandler(evt) {
-//   const currentElement = evt.target.closest('.element');
-//   currentElement.remove();
-// };
-
-// function setEventListeners() {
-//   const el = elementsTemplate.querySelector('.element').cloneNode(true);
-  // const deleteButton = el.querySelector('.element__delete');
-//   deleteButton.addEventListener('click', elementDeleteHandler);
-// };
-
-// const elementLike = document.querySelector('.element__like');
-
-// elementLike.addEventListener('click', function (evt) {
+// function likeHandler(evt) {
 //   const elementLike = evt.target.closest('.element__like');
 //   elementLike.classList.toggle('.element__like_active');
-// });
-
-// deleteButton.addEventListener('click', elementDeleteHandler);
+// };
 
 
